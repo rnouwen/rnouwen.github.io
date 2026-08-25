9da690d8db2504d3f8ecdfedf4acb0f5faa0f1c5
rnouwen <rnouwen@gmail.com>
2026-08-25 08:58:27 +0000

    Deploying to gh-pages from @ rnouwen/rnouwen.github.io@756efcecd734d6f75bb339a4eb638c641df74308 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-research",
-          title: "research",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
-        },{id: "nav-papers",
+  },{id: "nav-papers",
           title: "papers",
           description: "",
           section: "Navigation",
